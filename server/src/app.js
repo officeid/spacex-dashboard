a6593ad9a8ce1aa368e74fec932dd5110a5d8178
mfaisal <mfaisal@pnec.nust.edu.pk>
2021-10-16 18:22:59 +0500

    deploy cmd added

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { planetRouters } = require('./routes/planets/planets.routes');
 
 const app = express();
@@ -9,6 +10,11 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')));
 app.use(planetRouters);
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'..','public'));
+});
 
 module.exports = app;
